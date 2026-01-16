46b6cfcd72234fde60b4322b51d3b8947e6d3ff2
Venu Koka <68670438+Venu-Koka-Dev@users.noreply.github.com>
2026-01-16 09:10:23 +0530

    Update authMiddleware.ts

--- apps/product-service/src/middleware/authMiddleware.ts
+++ apps/product-service/src/middleware/authMiddleware.ts
@@ -10,42 +10,28 @@ declare global {
   }
 }
 
-export const shouldBeUser = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+
+export const shouldBeUser = (req: Request, res: Response, next: NextFunction) => {
   const auth = getAuth(req);
   const userId = auth.userId;
-
   if (!userId) {
     return res.status(401).json({ message: "You are not logged in!" });
   }
-
   req.userId = auth.userId;
-
   return next();
 };
 
-export const shouldBeAdmin = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+
+export const shouldBeAdmin = (req: Request, res: Response, next: NextFunction) => {
   const auth = getAuth(req);
   const userId = auth.userId;
-
   if (!userId) {
     return res.status(401).json({ message: "You are not logged in!" });
   }
-
   const claims = auth.sessionClaims as CustomJwtSessionClaims;
-
   if (claims.metadata?.role !== "admin") {
     return res.status(403).send({ message: "Unauthorized!" });
   }
-
   req.userId = auth.userId;
-
   return next();
 };
