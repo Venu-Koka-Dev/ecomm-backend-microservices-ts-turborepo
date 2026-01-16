5bf5bb511e1f18114744979b8244889b63bd3749
Venu Koka <68670438+Venu-Koka-Dev@users.noreply.github.com>
2026-01-16 09:13:52 +0530

    Update authMiddleware.ts

--- apps/order-service/src/middleware/authMiddleware.ts
+++ apps/order-service/src/middleware/authMiddleware.ts
@@ -8,32 +8,24 @@ declare module "fastify" {
   }
 }
 
-export const shouldBeUser = async (
-  request: FastifyRequest,
-  reply: FastifyReply
-) => {
+
+export const shouldBeUser = async (request: FastifyRequest, reply: FastifyReply) => {
   const { userId } = Clerk.getAuth(request);
   if (!userId) {
     return reply.status(401).send({ message: "You are not logged in!" });
   }
-
   request.userId = userId;
 };
 
-export const shouldBeAdmin = async (
-  request: FastifyRequest,
-  reply: FastifyReply
-) => {
+
+export const shouldBeAdmin = async (request: FastifyRequest, reply: FastifyReply) => {
   const auth = Clerk.getAuth(request);
   if (!auth.userId) {
     return reply.status(401).send({ message: "You are not logged in!" });
   }
-
   const claims = auth.sessionClaims as CustomJwtSessionClaims;
-
   if (claims.metadata?.role !== "admin") {
     return reply.status(403).send({ message: "Unauthorized!" });
   }
-
   request.userId = auth.userId;
 };
